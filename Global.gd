74b1f7a7ee3b141ebf3c0d12ccc947954946f287
Isma <xisma@protonmail.com>
2026-03-02 23:17:24 +0100

    monedas

--- Global.gd
+++ Global.gd
@@ -28,6 +28,11 @@ const SAVE_PATH = "user://savegame.save"
 
 func _ready():
 	load_game()
+	
+	# --- TRUCO DE DINERO INFINITO (BORRAR LUEGO) ---
+	agregar_monedas(1000000)
+	# -----------------------------------------------
+	
 	# Aplicamos el audio nada más cargar
 	aplicar_audio_guardado()
 
